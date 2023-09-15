a9e3c1c4408c83fba5a225e905f4f4035cf114e6
FullhouseGC <jimmyvdb96@outlook.com>
2023-09-15 12:40:44 +0100

    Added hovering effect so grid cells changes color when hovering over them

--- javascript.js
+++ javascript.js
@@ -1,7 +1,17 @@
-const container = document.querySelector('#container');
+let container = document.querySelector('#container');
 
-for(let i = 0; i < 256; i++) {
+//Grid creation
+let userInput = 144;
+for(let i = 0; i < userInput; i++) {
 const grid = document.createElement('div');
 grid.classList.add('grid');
 container.appendChild(grid);
 }
+
+//Grid coloring
+var grid = document.getElementsByClassName("grid")
+for(let i = 0; i < grid.length; i++){
+    grid[i].addEventListener("mousedown", function(){grid[i].style.backgroundColor = "blue";});
+    grid[i].addEventListener("mouseenter", function(){grid[i].style.backgroundColor = "blue";});
+}
+
