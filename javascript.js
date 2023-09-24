f3708651be143792bff6777e53c0796903c182be
FullhouseGC <jimmyvdb96@outlook.com>
2023-09-24 23:59:52 +0100

    Created prompt where user can isert custom grid size

--- javascript.js
+++ javascript.js
@@ -1,8 +1,24 @@
 let container = document.querySelector('#container');
 
+function userPrompt(){
+    container.replaceChildren();
+    let userInput = prompt("Enter grid size. Max 100");
+    let prompInt = parseInt(userInput);
+    let cellSelec = prompInt * prompInt;
+    for(let i = 0; i < cellSelec; i++) {
+        const grid = document.createElement('div');
+        grid.classList.add('grid');
+        container.appendChild(grid);
+        }
+        for(let i = 0; i < grid.length; i++){
+            grid[i].addEventListener("mouseenter", function(){grid[i].style.backgroundColor = "blue";});
+        }
+container.style.setProperty("--grid-template-columns", `repeat(${prompInt}, 1fr)`);
+container.style.setProperty("--grid-template-rows", `repeat(${prompInt}, 1fr)`);
+}
+
 //Grid creation
-let userInput = 144;
-for(let i = 0; i < userInput; i++) {
+for(let i = 0; i < 256; i++) {
 const grid = document.createElement('div');
 grid.classList.add('grid');
 container.appendChild(grid);
@@ -14,4 +30,6 @@ for(let i = 0; i < grid.length; i++){
     grid[i].addEventListener("mousedown", function(){grid[i].style.backgroundColor = "blue";});
     grid[i].addEventListener("mouseenter", function(){grid[i].style.backgroundColor = "blue";});
 }
+
+
 
