20de23bc16e50878909b464fdb80c9762d1c9eae
FullhouseGC <jimmyvdb96@outlook.com>
2023-09-25 19:21:31 +0100

    Setted a limit for grid creation to 100 max

--- javascript.js
+++ javascript.js
@@ -1,10 +1,16 @@
 let container = document.querySelector('#container');
+var grid = document.getElementsByClassName("grid")
 
+//Custom grid creation with coloring EventListener
 function userPrompt(){
     container.replaceChildren();
     let userInput = prompt("Enter grid size. Max 100");
     let prompInt = parseInt(userInput);
     let cellSelec = prompInt * prompInt;
+    if(prompInt > 100 || prompInt === 1){
+        return alert("Max is 100, minimum is 2")
+    }
+    else{
     for(let i = 0; i < cellSelec; i++) {
         const grid = document.createElement('div');
         grid.classList.add('grid');
@@ -13,23 +19,21 @@ function userPrompt(){
         for(let i = 0; i < grid.length; i++){
             grid[i].addEventListener("mouseenter", function(){grid[i].style.backgroundColor = "blue";});
         }
+    }
 container.style.setProperty("--grid-template-columns", `repeat(${prompInt}, 1fr)`);
 container.style.setProperty("--grid-template-rows", `repeat(${prompInt}, 1fr)`);
 }
 
-//Grid creation
+//Initial 16x 16 grid creation
 for(let i = 0; i < 256; i++) {
 const grid = document.createElement('div');
 grid.classList.add('grid');
 container.appendChild(grid);
 }
 
-//Grid coloring
+//Initial grid coloring EventListener
 var grid = document.getElementsByClassName("grid")
 for(let i = 0; i < grid.length; i++){
     grid[i].addEventListener("mousedown", function(){grid[i].style.backgroundColor = "blue";});
     grid[i].addEventListener("mouseenter", function(){grid[i].style.backgroundColor = "blue";});
 }
-
-
-
