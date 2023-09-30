296b0e406b2e9930165977703ea5e250bf709ec5
FullhouseGC <jimmyvdb96@outlook.com>
2023-09-30 17:30:47 +0100

    Changed the prompt to a slider to get user value of grid size

--- javascript.js
+++ javascript.js
@@ -1,19 +1,14 @@
 let container = document.querySelector('#container');
 var grid = document.getElementsByClassName("grid")
-function randomRgb(max) {
-    return Math.floor(Math.random() * max);
+function randomRgb() {
+    return Math.floor(Math.random() * 255);
   }
-  
-//Custom grid creation with coloring EventListener
-function userPrompt(){
+
+//Custom grid creation with coloring 
+function sliderValue(){
     container.replaceChildren();
-    let userInput = prompt("Enter grid size. Max 100");
-    let prompInt = parseInt(userInput);
-    let cellSelec = prompInt * prompInt;
-    if(prompInt > 100 || prompInt === 1){
-        return alert("Max is 100, minimum is 2")
-    }
-    else{
+    let slider = document.getElementById("myRange").value;
+    let cellSelec = slider * slider;
     for(let i = 0; i < cellSelec; i++) {
         const grid = document.createElement('div');
         grid.classList.add('grid');
@@ -22,17 +17,16 @@ function userPrompt(){
         for(let i = 0; i < grid.length; i++){
             grid[i].addEventListener("mouseenter", function(){grid[i].style.backgroundColor = "blue";});
         }
-    }
-container.style.setProperty("--grid-template-columns", `repeat(${prompInt}, 1fr)`);
-container.style.setProperty("--grid-template-rows", `repeat(${prompInt}, 1fr)`);
+    container.style.setProperty("--grid-template-columns", `repeat(${slider}, 1fr)`);
+    container.style.setProperty("--grid-template-rows", `repeat(${slider}, 1fr)`);
+
 }
 
 //Rainbow color
 function rainbow(){
-for(let i = 0; i < grid.length; i++){
-    grid[i].addEventListener("mouseenter", function(){grid[i].style.backgroundColor = `rgb(${randomRgb(255)}, ${randomRgb(255)}, ${randomRgb(255)})`});
-}
-
+    for(let i = 0; i < grid.length; i++){
+        grid[i].addEventListener("mouseenter", function(){grid[i].style.backgroundColor = `rgb(${randomRgb()}, ${randomRgb()}, ${randomRgb()})`});
+    }
 }
 
 //Initial 16x 16 grid creation
@@ -42,9 +36,11 @@ grid.classList.add('grid');
 container.appendChild(grid);
 }
 
-//Initial grid coloring EventListener
+//Initial grid coloring
 var grid = document.getElementsByClassName("grid")
 for(let i = 0; i < grid.length; i++){
     grid[i].addEventListener("mousedown", function(){grid[i].style.backgroundColor = "blue";});
     grid[i].addEventListener("mouseenter", function(){grid[i].style.backgroundColor = "blue";});
 }
+
+
