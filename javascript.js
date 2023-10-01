cf44e159beed25856ca10205efb50f1501c694b3
FullhouseGC <jimmyvdb96@outlook.com>
2023-10-01 23:17:15 +0100

    Added some style to the page

--- javascript.js
+++ javascript.js
@@ -4,10 +4,15 @@ function randomRgb() {
     return Math.floor(Math.random() * 255);
   }
 
+let rainbowIcon = document.getElementById("rainbowIcon");
+function rainbowColors(){
+    rainbowIcon.style.color = `rgb(${randomRgb()}, ${randomRgb()}, ${randomRgb()})`
+}
 //Custom grid creation with coloring 
 function sliderValue(){
     container.replaceChildren();
     let slider = document.getElementById("myRange").value;
+    document.getElementById("displayValue").textContent = slider;
     let cellSelec = slider * slider;
     for(let i = 0; i < cellSelec; i++) {
         const grid = document.createElement('div');
@@ -15,7 +20,7 @@ function sliderValue(){
         container.appendChild(grid);
         }
         for(let i = 0; i < grid.length; i++){
-            grid[i].addEventListener("mouseenter", function(){grid[i].style.backgroundColor = "blue";});
+            grid[i].addEventListener("mouseenter", function(){grid[i].style.backgroundColor = "rgb(63, 123, 201)";});
         }
     container.style.setProperty("--grid-template-columns", `repeat(${slider}, 1fr)`);
     container.style.setProperty("--grid-template-rows", `repeat(${slider}, 1fr)`);
@@ -27,9 +32,11 @@ function rainbow(){
     for(let i = 0; i < grid.length; i++){
         grid[i].addEventListener("mouseenter", function(){grid[i].style.backgroundColor = `rgb(${randomRgb()}, ${randomRgb()}, ${randomRgb()})`});
     }
+    setInterval(rainbowColors, 200);
 }
 
 //Initial 16x 16 grid creation
+document.getElementById("displayValue").textContent = 16;
 for(let i = 0; i < 256; i++) {
 const grid = document.createElement('div');
 grid.classList.add('grid');
@@ -39,8 +46,8 @@ container.appendChild(grid);
 //Initial grid coloring
 var grid = document.getElementsByClassName("grid")
 for(let i = 0; i < grid.length; i++){
-    grid[i].addEventListener("mousedown", function(){grid[i].style.backgroundColor = "blue";});
-    grid[i].addEventListener("mouseenter", function(){grid[i].style.backgroundColor = "blue";});
+    grid[i].addEventListener("mousedown", function(){grid[i].style.backgroundColor = "rgb(63, 123, 201)";});
+    grid[i].addEventListener("mouseenter", function(){grid[i].style.backgroundColor = "rgb(63, 123, 201)";});
 }
 
 
